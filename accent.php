ba81af218d9586c9f98368b3af4d10d181e97001
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-06-13 16:05:12 +0530

    Till 10

--- accent.php
+++ accent.php
@@ -40,43 +40,73 @@ $text = array($first);
 $storedata=array();
 
 
-/* pATalApAlaGkAmbAsAgarArthAnAm (1.2) */
-if (sub(array("pAwalA","surUpA","pAkalA","apAlaNka","Arevata","AragvaDa","mAtA","sAraga","samudra"),blank(0),blank(0),0))
+/* pATalApAlaGkAmbAsAgarArthAnAm (2) */
+if (ends($text,array("pAwalA","surUpA","pAkalA","apAlaNka","Arevata","AragvaDa","mAtA","sAraga","samudra"),1))
 {
-	$text = antodAtta();
-	storedata('PiwsUtra-1.2','sa',0);
+	$text = antodAtta(0);
+	storedata('Pi-2','sa',0);
 }
-/* gehArthAnAmastriyAm (1.3) */
+/* gehArthAnAmastriyAm (3) */
 // enumeration of other members pending.
-elseif (sub(array("geha","gfha"),blank(0),blank(0),0))
+elseif (ends($text,array("geha","gfha"),1))
 {
-	$text = antodAtta();
-	storedata('PiwsUtra-1.3','sa',0);
+	$text = antodAtta(0);
+	storedata('Pi-3','sa',0);
 }
-/* gehArthAnAmastriyAm (1.3) */
+/* gehArthAnAmastriyAm (3) */
 // astriyAm iti jJApakAt
-elseif (sub(array("SAlA"),blank(0),blank(0),0))
+elseif (ends($text,array("SAlA"),1))
 {
-	$text = AdyudAtta();
-	storedata('PiwsUtra-1.3','sa',0);
+	$text = AdyudAtta(0);
+	storedata('Pi-3','sa',0);
 }
-/* gudasya ca (1.4) */
-elseif (sub(array("guda"),blank(0),blank(0),0))
+/* gudasya ca (4) */
+elseif (ends($text,array("guda"),1))
 {
-	$text = antodAtta();
-	storedata('PiwsUtra-1.4','sa',0);
+	$text = antodAtta(0);
+	storedata('Pi-4','sa',0);
 }
-/* dhyapUrvasya strIviSayasya (1.5) */
+/* dhyapUrvasya strIviSayasya (5) */
 elseif (arr($text,'/[Dy][A]/'))
 {
-	$text = antodAtta();
-	storedata('PiwsUtra-1.5','sa',0);
+	$text = antodAtta(0);
+	storedata('Pi-5','sa',0);
 }
-/* phiSo'nta udAttaH (1.1) */
+/* khAntasyAzmAdeH (6) */
+elseif (arr($text,'/[K][aA]$/') && !arr($text,'/^[Sm]/'))
+{
+	$text = antodAtta(0);
+	storedata('Pi-6','sa',0);
+}
+/* baMhizWavatsaratiSatTAntAnAm (7) */
+elseif (ends($text,array("baMhizWa","vatsara","ti","Sat","Ta","TA"),1))
+{
+	$text = antodAtta(0);
+	storedata('Pi-7','sa',0);
+}
+/* dakziRasya sADO (8) */
+elseif ($_GET['phit']==='1.1')
+{
+	$text = antodAtta(0);
+	storedata('Pi-8','sa',0);
+}
+/* svAGgAkhyAyAmAdirvA (9) */
+elseif ($_GET['phit']==='1.2')
+{
+	$text = AdyantodAttavA(0);
+	storedata('Pi-9','sa',0);
+}
+/* chandasi ca (10) */
+elseif ($_GET['phit']==='1.3')
+{
+	$text = AdyantodAttavA(0);
+	storedata('Pi-10','sa',0);
+}
+/* phiSo'nta udAttaH (1) */
 else
 {
 	$text = antodAtta();
-	storedata('PiwsUtra-1.1','sa',0);	
+	storedata('Pi-1','sa',0);	
 }
 
 
